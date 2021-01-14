ebd47990a009b1937d8b3debaf7b136a64170f69
cyha <68947506+cyha@users.noreply.github.com>
2021-01-14 22:09:41 +0100

    fin template bootstrap laravel8

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,55 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/404', function () {
+    return view('404');
+});
+
+Route::get('/blank', function () {
+    return view('blank');
+});
+
+Route::get('/buttons', function () {
+    return view('buttons');
+});
+
+Route::get('/cards', function () {
+    return view('cards');
+});
+
+Route::get('/charts', function () {
+    return view('charts');
+});
+
+Route::get('/forgot-password', function () {
+    return view('forgot-password');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('login');
+});
+
+Route::get('/tables', function () {
+    return view('tables');
+});
+
+Route::get('/utilities-animation', function () {
+    return view('utilities-animation');
+});
+
+Route::get('/utilities-border', function () {
+    return view('utilities-border');
+});
+
+Route::get('/utilities-other', function () {
+    return view('utilities-other');
+});
+
+Route::get('/utilities-color', function () {
+    return view('utilities-color');
+});
